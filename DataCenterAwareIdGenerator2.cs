4610d6926850d1066c93759c12d9976daa774d85
makka <claudio@jitidea.net>
2015-11-10 12:05:29 +0100

    the data-center identifier byte now is first

---
+++ DataCenterAwareIdGenerator2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectId2
+{
+    public class DataCenterAwareIdGenerator2 : MongoDB.Bson.Serialization.IIdGenerator
+    {
+        public static byte DataCenter { get; set; }
+
+
+        public static byte[] Pack(byte dataCenter, int timestamp, int machine, short pid, int increment)
+        {
+            if ((machine & 0xff000000) != 0)
+            {
+                throw new ArgumentOutOfRangeException("machine", "The machine value must be between 0 and 16777215 (it must fit in 3 bytes).");
+            }
+            if ((increment & 0xff000000) != 0)
+            {
+                throw new ArgumentOutOfRangeException("increment", "The increment value must be between 0 and 16777215 (it must fit in 3 bytes).");
+            }
+
+            byte[] bytes = new byte[12];
+            bytes[0] = dataCenter;
+            bytes[1] = (byte)(timestamp >> 24);
+            bytes[2] = (byte)(timestamp >> 16);
+            bytes[3] = (byte)(timestamp >> 8);
+            bytes[4] = (byte)(timestamp);
+            bytes[5] = (byte)(machine >> 16);
+            bytes[6] = (byte)(machine >> 8);
+            bytes[7] = (byte)(machine);
+            bytes[8] = (byte)(pid >> 8);
+            bytes[9] = (byte)(pid);
+            bytes[10] = (byte)(increment >> 16);
+            bytes[11] = (byte)(increment >> 8);            
+            return bytes;
+        }
+
+        public object GenerateId(object container, object document)
+        {
+            var newId = ObjectId.GenerateNewId();
+            var bytes = Pack(DataCenter, newId.Timestamp, DataCenter, newId.Pid, newId.Increment);
+            return new ObjectId(bytes).ToString();
+        }
+
+        public bool IsEmpty(object id)
+        {
+            return id == null || (ObjectId)id == ObjectId.Empty;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,11 +7,11 @@ If you want to split data on multiple shards using only the _id field
 ```js
 sh.enableSharding("demo");
 sh.shardCollection("demo.foo", { _id : 1 } );
-sh.addTagRange("demo.foo", { _id: MinKey }, { _id: ObjectId("000064000000000000000000") }, "DC1");
-sh.addTagRange("demo.foo", { _id: ObjectId("000064000000000000000000") }, { _id: MaxKey }, "DC2");
+sh.addTagRange("demo.foo", { _id: MinKey }, { _id: ObjectId("640000000000000000000000") }, "DC1");
+sh.addTagRange("demo.foo", { _id: ObjectId("640000000000000000000000") }, { _id: MaxKey }, "DC2");
 ```
 
-When the app starts you have to set the right value for the machine identifier to target data to desired data-center using only the _id field
+When the app starts you have to set the right value for the data-center identifier to target data to desired data-center using only the _id field
 ```csharp
 DataCenterAwareIdGenerator.DataCenter = 200;
 ```
